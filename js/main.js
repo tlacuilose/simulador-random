4f471259012668a48b615550d56da7fe44db7244
Jose Tlacuilo <65783495+tlacuilose@users.noreply.github.com>
2021-09-23 00:47:34 -0500

    Creates UI for about and generator views (#2)
    * Restructures project organization.
    * Adds about page.
    * Adds calculations and results sections.
    * Adds view structure of generators.

--- js/main.js
+++ js/main.js
@@ -5,15 +5,15 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 700,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  // and load the entry html of the app.
+  mainWindow.loadFile('html/about.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
