0c8a141dab5d29bb39b9923ebe5c19b1ac00b639
Jose Tlacuilo <65783495+tlacuilose@users.noreply.github.com>
2021-09-30 05:20:02 -0500

    Adds documentation. Creates a npm command:  npm run generate-docs (#10)

--- tests/unit/mcm.test.js
+++ tests/unit/mcm.test.js
@@ -5,25 +5,32 @@ test('creates a mcm generator', () => {
   expect(mcm.results).toEqual([]);
 });
 
+test('generate a five iterations random', () => {
+  let mcm = new Mcm();
+  let finalResult = mcm.generate(0, 0, 0, 1, 5);
+  expect(finalResult.randoms.length).toBe(5);
+})
+
 test('generate a zero value random', () => {
   let mcm = new Mcm();
-  let finalResult = mcm.generate(0, 0, 0, 0, 0);
+  let finalResult = mcm.generate(0, 0, 0, 1, 5);
+  let expected = [0, 0, 0, 0, 0];
+  expect(finalResult.randoms).toEqual(expected);
+})
+
+test('generate a zero iterations random', () => {
+  let mcm = new Mcm();
+  let finalResult = mcm.generate(0, 0, 0, 1, 0);
   expect(finalResult.randoms).toEqual([]);
 })
 
-test('generate class example random', () => {
-  let mcm = new Mcm();
-  let finalResult = mcm.generate(4, 5, 7, 8, 5);
-  expect(finalResult.randoms.at(-1)).toBe(7);
-})
-
-test('generate zero value random', () => {
+test('generate zero module random', () => {
   let mcm = new Mcm();
   let finalResult = mcm.generate(0, 0, 0, 0, 5);
   expect(finalResult.randoms).toEqual([]);
 })
 
-test('generate zero value random and throw error', () => {
+test('generate zero module random and throw error', () => {
   let mcm = new Mcm();
   let finalResult = mcm.generate(0, 0, 0, 0, 5);
   expect(finalResult.error).toBeDefined();
