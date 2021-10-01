bf8a1efee80b83a294e5e225f1ecd28845313fc2
Jose Tlacuilo <65783495+tlacuilose@users.noreply.github.com>
2021-10-01 04:34:24 -0500

    Tests  about view. (#14)

--- app/main.js
+++ app/main.js
@@ -35,9 +35,11 @@ app.whenReady().then(() => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
+/*
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
+*/
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
